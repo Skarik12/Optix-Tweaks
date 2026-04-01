b3d950faafcbab8140fcdbfd34b384bc4d9403f6
Skarik12 <151785266+Skarik12@users.noreply.github.com>
2026-03-31 21:36:41 -0700

    npm install cp .env.example .env npx prisma db push npm run dev

--- lib/token.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export const generateToken = () => crypto.randomBytes(32).toString("hex");
-export const sha256 = (input: string) =>
-  crypto.createHash("sha256").update(input).digest("hex");
